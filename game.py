e1d4068bcb248925dc212a787f934c5c843f1ed0
Robert Benavidez <murabitbenavidez@gmail.com>
2022-07-14 15:04:32 -0400

    capturing player input

--- game.py
+++ game.py
@@ -6,4 +6,16 @@ def welcome():
     longer, the villagers pooled their wealth and hired the most skilled adventurer they could find: you. After
     listening to their tale of woe, you agree to enter the labyrinth where most of the creatures seem to originate,
     and destroy the foul beasts. Armed with a longsword and a bundle of torches, you descend into the labyrinth, 
-    ready to do battle....""")
+    ready to do battle....
+    """)
+
+
+def play_game():
+    input('Pres ENTER to continue')
+    explore_labyrinth()
+
+
+def explore_labyrinth():
+    while True:
+        player_input = input('->')
+        print(player_input)
--- main.py
+++ main.py
@@ -4,6 +4,10 @@ import game
 def main():
     game.welcome()
 
+    # game loop: play the game until the game is over or the player quits
+    while True:
+        game.play_game()
+
 
 if __name__ == "__main__":
     main()

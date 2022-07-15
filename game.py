6dd1b8129d5ac7986f9185b708fe09ea50571076
Robert Benavidez <murabitbenavidez@gmail.com>
2022-07-15 12:37:59 -0400

    program quits with the quit input

--- game.py
+++ game.py
@@ -11,6 +11,7 @@ def welcome():
 
 
 def play_game():
+    welcome()
     input('Pres ENTER to continue')
     explore_labyrinth()
 
@@ -18,4 +19,27 @@ def play_game():
 def explore_labyrinth():
     while True:
         player_input = input('->')
-        print(player_input)
+
+        if player_input == 'help':
+            print('help goes here')
+        elif player_input == 'quit':
+            # TODO: print final score
+            print('Fleeing the Dungeon you leave the village in peril.')
+            play_again()
+
+        else:
+            print('Incorrect command. Please type help to find the proper commands')
+
+
+# restart game function
+# TODO: currently the function quites with any command other than 'yes'
+def play_again():
+    yn = input('Play again? (yes/no) ->')
+
+    if yn == 'yes':
+        play_game()
+    else:
+        print('Until next time...')
+        exit(0)
+
+
--- main.py
+++ main.py
@@ -2,11 +2,9 @@ import game
 
 
 def main():
-    game.welcome()
-
     # game loop: play the game until the game is over or the player quits
-    while True:
-        game.play_game()
+
+    game.play_game()
 
 
 if __name__ == "__main__":
